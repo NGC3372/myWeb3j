cafecbbed4000cbe8820d9e3ffc91189a13c79b2
guohao <49350058+NGC3372@users.noreply.github.com>
2022-01-13 20:02:39 +0800

    update

--- src/main/kotlin/callContract.kt
+++ src/main/kotlin/callContract.kt
@@ -6,11 +6,11 @@ fun main() {
     val web3 = myWeb3.localWeb3
     val account = myWeb3.localAccount
     val provider: ContractGasProvider = StaticGasProvider(BigInteger("100000"), BigInteger("200000"))
-    val contract = Storage_sol_Storage.load("0xcef89cd42586a565f640196e2155575f37a43cb8",
+    val contract = Storage_sol_Storage.load("0xcacb96ce2beb943c0069b46c6e475543f76017be",
         web3,account,provider)
 
-    val recipt = contract.store(BigInteger("100000")).send()
-    println("Txhash: ${recipt.transactionHash}")
+    //val recipt = contract.store(BigInteger("100000")).send()
+    //println("Txhash: ${recipt.transactionHash}")
     println("===========================")
     println("result value: ${contract.retrieve().send()}")
 }
--- src/main/kotlin/deployContract.kt
+++ src/main/kotlin/deployContract.kt
@@ -1,3 +1,5 @@
+import org.web3j.protocol.core.DefaultBlockParameter
+import org.web3j.protocol.core.DefaultBlockParameterName
 import org.web3j.tx.gas.ContractGasProvider
 import org.web3j.tx.gas.StaticGasProvider
 import java.math.BigInteger
@@ -6,7 +8,9 @@ import java.math.BigInteger
 fun main() {
     val web3 = myWeb3.localWeb3
     val account = myWeb3.localAccount
-    val provider: ContractGasProvider = StaticGasProvider(BigInteger("100000"), BigInteger("200000"))
+    println(account.address)
+    println(web3.ethGetBalance(account.address, DefaultBlockParameterName.LATEST).send().balance)
+    val provider: ContractGasProvider = StaticGasProvider(BigInteger("100000000000"), BigInteger("2000000"))
     val request = Storage_sol_Storage.deploy(web3,account,provider)
     println(request.send().contractAddress)
 }
